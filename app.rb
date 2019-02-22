21d9bdc5aa2bf929ef401ce315d10f7b520c1ed8
karamalesa <karamalesa@gmail.com>
2019-02-22 21:05:39 +0200

    Chenge My website Karamalesa TV

--- app.rb
+++ app.rb
@@ -4,5 +4,17 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!</a>"			
+	erb "Hello! <a href=\"https://github.com/karamalesa1980/lesson22\">Original</a> pattern has been modified for <a href=\"https://www.youtube.com/channel/UCiqUSuswB1_uU2BONwvCTYg?view_as=subscriber\">Karamalesa TV</a>"			
 end
+
+get '/contacts' do
+	erb :contacts
+end	
+
+get '/blabla' do
+	erb :blabla
+end	
+
+get '/something' do
+	erb :something
+end	
